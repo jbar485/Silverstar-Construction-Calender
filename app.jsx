0f5958c367d678ce6c533950a638cc3c63c5ba63
Judah Barton <judah.barton@yahoo.com>
2020-02-28 12:28:07 -0800

    update Readme.md

---
+++ app.jsx
@@ -0,0 +1,11 @@
+const header = (
+  <header>
+  <h1>My First React Element!</h1>
+  <p>I just learned how to create a React node and render it in to the dom</p>
+  </header>
+);
+
+ReactDOM.render(
+  header,
+  document.getElementById('root')
+);
